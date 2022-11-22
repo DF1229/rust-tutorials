61732bd1001b9c3e4c96a4509d564d6f19fd276c
Daan Faber <daan.faber001@fclive.nl>
2022-11-22 11:31:01 +0100

    Removed println!'s

--- Chapter 12 - IO CLI project/minigrep/src/lib.rs
+++ Chapter 12 - IO CLI project/minigrep/src/lib.rs
@@ -20,7 +20,9 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
+    let _contents = fs::read_to_string(config.file_path)?;
+    Ok(())
+}
 
     println!("With text:\n{contents}");
     Ok(())
