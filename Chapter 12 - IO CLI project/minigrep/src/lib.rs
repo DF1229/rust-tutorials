c20eefb61611355d7ee7194149f993d53a56f557
Daan Faber <daan.faber001@fclive.nl>
2022-11-22 11:31:32 +0100

    simple searching functionality

--- Chapter 12 - IO CLI project/minigrep/src/lib.rs
+++ Chapter 12 - IO CLI project/minigrep/src/lib.rs
@@ -24,6 +24,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line.trim());
+        }
+    }
+    
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
